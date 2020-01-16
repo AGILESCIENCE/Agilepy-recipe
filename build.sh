d64f23b6137b4eda76ea641fb252471f3c6228d6
antonioaddis <59084137+antonioaddis@users.noreply.github.com>
2020-01-16 10:19:05 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,27 @@
+export CFITSIO=$PREFIX
+export ROOTSYS=$PREFIX
+export AGILE=$PREFIX/share/agiletools
+export C_INCLUDE_PATH=$PREFIX/include
+export CPP_INCLUDE_PATH=$PREFIX/include
+export ZLIBPATH=$PREFIX/lib
+
+export PFILES=$PFILES:$AGILE/share
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+export LIBRARY_PATH=$PREFIX/lib:$LIBRARY_PATH
+export PATH=$AGILE/bin:$AGILE/scripts:$PATH
+
+
+
+echo "LD_LIBRARY_PATH= $LD_LIBRARY_PATH"
+echo "LIBRARY_PATH= $LIBRARY_PATH"
+mkdir $PREFIX/share/agiletools
+git clone https://github.com/AGILESCIENCE/AGILE-GRID-ScienceTools-Setup.git
+cd AGILE-GRID-ScienceTools-Setup
+git checkout master
+./downloadScienceTools_notag.sh
+./installScienceTools.sh
+./downloadIRFConda.sh
+./installIRFConda.sh
+
+cd $PREFIX/../work
 $PYTHON setup.py install
