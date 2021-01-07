aca129546376c834e2a813589aec5a26e3ae8688
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2021-01-07 18:41:06 +0100

    New Agilepy's datasets

--- agilepy-env/local-build/build.sh
+++ agilepy-env/local-build/build.sh
@@ -31,3 +31,19 @@ git checkout "$SCIENCE_TOOLS_TAG"
 ##########################################
 cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/activate_${condaname}.sh
 cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/deactivate_${condaname}.sh
+
+
+##########################################
+# Downloading the datasets
+##########################################
+mkdir -p $AGILE/agilepy-test-data
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1B31SCrHoOU0KnZoaZ7NTq6nY_PTD-ner' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1B31SCrHoOU0KnZoaZ7NTq6nY_PTD-ner" -O test_dataset_6.0.tar.gz && rm -rf /tmp/cookies.txt
+tar -xzf test_dataset_6.0.tar.gz
+mv test_dataset_6.0 $AGILE/agilepy-test-data
+rm test_dataset_6.0.tar.gz
+
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1B3Tp-01-X7Cwh6lq11BUCiaHuctj0iDW' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1B3Tp-01-X7Cwh6lq11BUCiaHuctj0iDW" -O test_dataset_agn.tar.gz && rm -rf /tmp/cookies.txt
+tar -xzf test_dataset_agn.tar.gz
+mv test_dataset_agn $AGILE/agilepy-test-data
+rm test_dataset_agn.tar.gz
