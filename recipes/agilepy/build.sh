fe77010568bc9f5db6e285b2cdff3b4fd1bdee40
Antonio Addis <antonio.addis@inaf.it>
2021-01-20 10:23:57 +0100

    mkdir and meta.yaml updated

--- recipes/agilepy/build.sh
+++ recipes/agilepy/build.sh
@@ -27,6 +27,8 @@ $PYTHON setup.py install
 # Copying files
 ##########################################
 mkdir -p $AGILE/scripts
+mkdir -p $PREFIX/etc/conda/activate.d/
+mkdir -p $PREFIX/etc/conda/deactivate.d/
 cp $RECIPE_DIR/activate.sh $PREFIX/etc/conda/activate.d/activate_${condaname}.sh
 cp $RECIPE_DIR/deactivate.sh $PREFIX/etc/conda/deactivate.d/deactivate_${condaname}.sh
 cp $SRC_DIR/agilepy/scripts/get_agilepy_path.py $AGILE/scripts
