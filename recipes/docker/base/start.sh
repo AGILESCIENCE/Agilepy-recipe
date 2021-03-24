4ec2013466b131c3022a3a78a0c05d34dd3fb557
Antonio Addis <antonio.addis@inaf.it>
2021-03-24 19:34:59 +0100

    updated dockerfile and script

---
+++ recipes/docker/base/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p shared_dir &&
+
+docker run --rm -it -p 8888:8888 \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v $PWD/shared_dir:/shared_dir \
+    agilescience/agilepy-recipe:latest
